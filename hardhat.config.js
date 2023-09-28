6d0d7bf2377c0146a963f42205cdebcc3a8c817c
SahilPanhotra <sahilpanhotra@gmail.com>
2023-09-28 15:18:56 +0530

    mint and transfer using swisstronic

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
+      accounts: [`0x${process.env.PRIVATE_KEY}`], //Your private key starting with "0x"
+    },
+  },
+};
